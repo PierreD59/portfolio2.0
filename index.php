ec936dc6bfd72ba0a2ec17249d02b5c73faa8098
Pierre Dumont <pierre.dmt59@gmail.com>
2018-09-22 18:04:04 +0200

    First commit with base boilerplate and first part of html css JS and PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include('header.php'); ?>
+
+<div class="col-12 d-flex container-fluid m-0 p-0">
+  <div class="col-12 d-flex row m-0 p-0">
+    <div class="image-apropos img-fluid">
+      <img src="#" alt="photo">
+    </div>
+    <div class="col-10 a_propos">
+      <h2 class="mb-3 mt-3 text-center w-100">A propos de moi</h2>
+        <p class="text-justify text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+  </div>
+</div>
+
+<?php include('footer.php'); ?>
